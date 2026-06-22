c7721927a79101ddaaf0ecaf783d7bdbfe2bbb17
Shahad Mahmud <shahad@Shahads-MacBook-Pro.local>
2026-06-22 18:47:29 +0600

    feat: inject the widget via wp_footer

--- includes/frontend.php
+++ includes/frontend.php
@@ -2,3 +2,22 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+add_action( 'wp_footer', 'iterochat_print_widget' );
+function iterochat_print_widget() {
+	if ( is_admin() ) {
+		return;
+	}
+	if ( ! iterochat_is_connected() ) {
+		return;
+	}
+	$conn = iterochat_get_connection();
+	if ( empty( $conn['enabled'] ) || empty( $conn['widget_key'] ) ) {
+		return;
+	}
+	printf(
+		'<script src="%s/widget.js" data-widget-key="%s" async></script>' . "\n",
+		esc_url( iterochat_widget_url() ),
+		esc_attr( $conn['widget_key'] )
+	);
+}
